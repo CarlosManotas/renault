64f313ca52064815c7ea71921995573869e4346d
GERSON <GERSON>
2016-01-20 21:10:21 -0300

    layout muestra

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(function(){
+	var $figure = $("figure");
+	//retornar todos los figure
+	var $playerVideo = $("#player-video");
+	var hijos = $playerVideo.find("figure");
 
+	/*for(var tete in hijos){
+		var tamano = $(hijos[tete]).css("background-image");
+		console.log(tamano);
+	}*/
+	function mostrar_propiedades(objeto, nombreObjeto) {
+	   var resultado = "";
+	   for (var i in objeto) {
+	      resultado += nombreObjeto + "." + i + " = " + objeto[i] + "\n";
+	   }
+	   console.log(resultado) ;
+	}
+	mostrar_propiedades(hijos , "manotas");
 });
